f4468abe5b26a7ad209748f5857c6e6089ba2e00
Tomaž Stepišnik Perdih <tomaz.stepi@gmail.com>
2014-11-27 14:39:54 +0100

    Dodani komentarji v datoteki NQueens.hs

--- Koda iz članka/NQueens.hs
+++ Koda iz članka/NQueens.hs
@@ -1,52 +1,71 @@
+-- | Board size.
 n = 8
 
-type Move = Int 
-type Position = (Move, Move)
+-- | Move is the number of column in which a queen is placed.
+type Move =  Int
+
+-- | Coordinates on the board.
+type Position = (Int, Int)
+
+-- | Type of truth values that represent game outcomes.
 type R = Bool
 
+-- | Call @attacks p1 p2@ checks if a queen in position @p1@ attacks position @p2@.
 attacks :: Position -> Position -> Bool
 attacks (x, y) (a, b) = x == a  ||  y == b  ||  abs(x - a) == abs(y - b)
 
+-- | Function that checks if placement of queens on given positions is valid.
 valid :: [Position] -> Bool
 valid [] = True
 valid (u : vs) =  not(any (\v -> attacks u v) vs) && valid vs
 
+-- | Predicate on move sequences which tests the validity of resulting queen placement.
 p :: [Move] -> R
 p ms = valid (zip ms [0..(n-1)])
 
+-- to spet importava
 type J r x = (x -> r) -> x
 
+-- | List of history depentent selection functions of moves, 
+-- where k-th selection function returns optimal move on k-th turn.
 epsilons :: [[Move] -> J R Move]
 epsilons = replicate n epsilon
   where epsilon h = find ([0..(n-1)] `setMinus` h)
 
+-- to tut
 find :: [x] -> J Bool x
-find []     p = undefined
-find [x]    p = x
-find (x:xs) p = if p x then x else find xs p
+find []     q = undefined
+find [x]    q = x
+find (x:xs) q = if q x then x else find xs q
 
+-- to tut
 otimes :: J r x -> (x -> J r [x]) -> J r [x]
 otimes e0 e1 p = a0 : a1
   where a0 = e0(\x0 -> overline (e1 x0) (\x1 -> p(x0:x1)))
         a1 = e1 a0 (\x1 -> p(a0 : x1))
         overline e p = p(e p)
 
+-- to tut
 bigotimes :: [[x] -> J r x] -> J r [x]
 bigotimes [] = \p -> []
 bigotimes (e : es) =  
  e [] `otimes` (\x -> bigotimes[\xs->d(x:xs) | d <- es])
 
+-- |Optimal play for NQueens.
 optimalPlay :: [Move]
 optimalPlay = bigotimes epsilons p
 
+-- | Optimal outcome of NQueens
 optimalOutcome :: R
 optimalOutcome = p optimalPlay
 
+-- | Returns an optimal move, given previous moves.
 optimalStrategy :: [Move] -> Move
 optimalStrategy as = head(bigotimes epsilons' p')
  where epsilons' = drop (length as) epsilons
        p' xs = p(as ++ xs)
 
+-- | Function that removes an element from an ordered list.
 delete :: Ord x => x -> [x] -> [x]
 delete x [] = []
 delete x (vs@(y : ys))
@@ -54,6 +73,7 @@ delete x (vs@(y : ys))
     | x <  y    = vs
     | otherwise = y : delete x ys 
 
+-- | Call @setMinus xs ys@ returns a list with elements of @xs@ that are not contained in @ys@.
 setMinus :: Ord x => [x] -> [x] -> [x]
 setMinus xs [] = xs
 setMinus xs (y : ys) = setMinus (delete y xs) ys
