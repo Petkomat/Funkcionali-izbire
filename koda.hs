44d1002bf801f81e9feac472b745f585f22c61d5
Tomaž Stepišnik Perdih <tomaz.stepi@gmail.com>
2015-01-08 10:42:11 +0100

    Dodala memoizacijo za hitrejse racunanje.

--- koda.hs
+++ koda.hs
@@ -21,22 +21,21 @@ p12 n = True
 p13 n = False
 p14 n = n > 10
 
+
 -- Nelahek primer
 g :: K Baire -> J Baire
 g fi p = if fi p
-		 then g' fi p
+		 then g' 
 		 else g fi (\x -> True)
-		 
-g' :: (K Baire) -> (Baire -> Bool) -> Baire
-g' fi p n = miniT fi p n 0
+	where g' = (map g'' [0 ..] !!)
+		where g'' n = miniT n 0
+			where miniT n t = if fi (\b -> (prvihN b g' n) && b n == t && p b)
+							  then t
+							  else miniT n (t+1)
 
 prvihN :: Baire -> Baire -> Nat -> Bool
 prvihN _ _ 0 = True
 prvihN a b n = (a (n-1) == b (n-1)) && prvihN a b (n-1)
-
-miniT fi p n t = if fi (\b -> (prvihN b (g' fi p) n) && b n == t && p b)
-				 then t
-				 else miniT fi p n (t+1)
 
 
 testFi p = p (\n -> 2*n) || p (\n -> n+1) || p(\n -> 42)
