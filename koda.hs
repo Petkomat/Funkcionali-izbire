2e53dbde1f20478eb2664a820955766593afc9ad
Tomaž Stepišnik Perdih <tomaz.stepi@gmail.com>
2015-01-15 15:14:44 +0100

    dodan še zadnji primer. casovna zahtevnost trenutno obupna.

--- koda.hs
+++ koda.hs
@@ -86,43 +86,53 @@ r :: K Baire -> Baire -> Baire
 r fi a = \n -> if fi (\b -> (prvihN b (r fi a) n) && b n == a n)
 			   then a n
 			   else miniM n 0
-				where miniM n m = if fi (\b -> (prvihN b (r fi a) n) && b n == m )
-								  then m
-								  else miniM n (m+1)
+					where miniM n m = if fi (\b -> (prvihN b (r fi a) n) && b n == m )
+									  then m
+									  else miniM n (m+1)
 								  
--- eE :: K (Baire -> Nat) -> Baire -> (Baire -> Nat)
--- eE fi a = \x -> miniY x 0 
-			-- where miniY x y = if fi (\f -> (prvihN (pP f) (r (\p -> fi (p . pP)) a) (miniN x a 0)) && f x == y)
-							  -- then y
-							  -- else miniY x (y+1)
-				-- where miniN x a n = if forall f $ forall g $ prvihN a (P f) (P g) n -> f x = g x
-									-- then n
-									-- else miniN x a (n+1)
+eE :: K (Baire -> Nat) -> Baire -> (Baire -> Nat)
+eE fi a = \x -> miniY x 0 where
+						miniY x y = (if fi (\f -> (prvihN (pP f) (r (\p -> fi (p . pP)) a) (miniN x a 0)) && f x == y)
+									 then y
+									 else miniY x (y+1))
+						miniN x a n = (if forall (\f -> forall (\g -> not (prvihNa a (pP f) (pP g) n) || f x == g x))
+									   then n
+									   else miniN x a (n+1))
+						forall p = not (fi (\b -> not (p b)))
+						prvihNa a f g 0 = True
+						prvihNa a f g n = prvihNa a f g (n-1) && (f (n-1) == g (n-1) ) && (f (n-1) == a (n-1))
 							  
--- h :: K (Baire -> Nat) -> J (Baire -> Nat)
--- h fi p = eE (g (\p -> fi (p . pP)) (p . eE))
+h :: K (Baire -> Nat) -> J (Baire -> Nat)
+h fi p = eE fi f'
+	where f' = g (\p -> fi (p . pP)) (\y -> p (eE fi y))
 
+fiNNN :: ((Baire -> Nat) -> Bool) -> Bool 
+fiNNN p = p (\f -> 0) || p (\f -> f 1)
+p31,p32,p33 :: (Baire -> Nat) -> Bool
+p31 = \fF -> fF (\n -> 1) == 0
+p32 = \fF -> fF (\n -> 1) == 2
+p33 = \fF -> fF (\n -> 1) == 1
 
 -- drugi poskus finda na Baire:
 -- Imamo gosto zaporednje d_n funkcij na Baire
 
-isciBaire fi p = if (fi p)
-					then pomo p 0
-					else isciBaire fi (\f -> True)
-						where pomo p n = if p (d n)
-											then d n
-											else pomo p (n + 1)
+-- isciBaire fi p = if (fi p)
+				 -- then pomo p 0
+				 -- else isciBaire fi (\f -> True)
+					-- where pomo p n = if p (d n)
+									 -- then d n
+									 -- else pomo p (n + 1)
 
 -- prvih 20 lst-ov
 -- [[0],[1],[0,0],[1,0],[2],[3],[2,0],[3,0],[0,1],[1,1],[0,0,0],[1,0,0],
 -- [2,1],[3,1],[2,0,0],[3,0,0],[4],[5],[4,0],[5,0]]
 -- opomba: d0 == d2, ampak je vseen ...
 
-pTest :: Baire -> Bool
-pTest = \f -> (f 0 == 2) && (f 1 == 0)
+-- pTest :: Baire -> Bool
+-- pTest = \f -> (f 0 == 2) && (f 1 == 0)
 
-fiTest :: (Baire -> Bool) -> Bool
-fiTest p = any p (map d [0..5]) -- vrne d_5
+-- fiTest :: (Baire -> Bool) -> Bool
+-- fiTest p = any p (map d [0..5]) -- vrne d_5
 
 
 -- tezak primer 2:
@@ -131,27 +141,27 @@ fiTest p = any p (map d [0..5]) -- vrne d_5
 -- Fn (g) = 1. najdi ii =  argmin g =_M d_i, 0 <= i <= n, kjer je M = max(length lst i)
 --			2. ce ii obstaja, vrni (d n) !! ii sicer 0
 
-maksiDolz :: Nat -> Nat
-maksiDolz n = if n == 0
-				then length $ lst 0
-				else max (maksiDolz (n-1)) (length $ lst n)
+-- maksiDolz :: Nat -> Nat
+-- maksiDolz n = if n == 0
+			  -- then length $ lst 0
+			  -- else max (maksiDolz (n-1)) (length $ lst n)
 				
-gostiF :: Nat -> (Baire -> Nat)
-gostiF n = \g -> (pomo g 0) 
-			where pomo g k | k > n = 0
-						   | k <= n && (foldl (&&) True (zipWith (==) (map (d k) [0..dolz]) (map g [0..]))) = d n k -- ker je lst ravno bijekcija Nat -> končni [Nat], bo d n k ok vrednost
-						   | otherwise = pomo g (k+1)
-						   where dolz = (maksiDolz n) - 1
+-- gostiF :: Nat -> (Baire -> Nat)
+-- gostiF n = \g -> (pomo g 0) 
+			-- where pomo g k | k > n = 0
+						   -- | k <= n && (foldl (&&) True (zipWith (==) (map (d k) [0..dolz]) (map g [0..]))) = d n k -- ker je lst ravno bijekcija Nat -> končni [Nat], bo d n k ok vrednost
+						   -- | otherwise = pomo g (k+1)
+						   -- where dolz = (maksiDolz n) - 1
 						   
 -- stestirano gostiF: na majhnih ok, tudi na n = 142 (lst n = [3, 1, 1])
 
-isciNNN fi p = if (fi p)
-					then pomo p 0
-					else isciNNN fi (\f -> True)
-						where pomo p n = if p (gostiF n)
-											then gostiF n
-											else pomo p (n + 1)
-											
+-- isciNNN fi p = \a -> if (fi p)
+				     -- then pomo 0
+				     -- else isciNNN fi (\f -> True)
+						-- where pomo n = if p (gostiF n)
+									   -- then gostiF n
+									   -- else pomo p (n + 1)
+												
 
 
 
